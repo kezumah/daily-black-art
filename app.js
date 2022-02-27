376aeefbca5f3493f9bacece3cdb67a3f1375fd8
kezumah <kennedy.ezumah@gmail.com>
2022-02-26 21:42:57 -0800

    setup frontend files

--- app.js
+++ app.js
@@ -39,3 +39,35 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+/*
+
+There's a lot of uneccessary information from above; remove this when I am ready to get started
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+// const indexRouter = require('./routes/index');
+const usersRouter = require("./routes/users");
+const pmResumesRouter = require("./routes/pm");
+const sweRouter = require("./routes/swe");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "frontend/build")));
+
+// app.use('/', indexRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/pm", pmResumesRouter);
+app.use("/api/swe", sweRouter);
+
+module.exports = app;
+*/
+
